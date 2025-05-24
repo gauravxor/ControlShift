6eb7bba97af0260c2fcfba6aa10b717a232887f5
Gaurav Aggarwal <gauravxor@gmail.com>
2025-05-24 05:45:43 +0530

    Remove DataSource exclusion code gateway service

--- gateway-service/src/main/java/com/clumsycoder/controlshift/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/clumsycoder/controlshift/gateway/GatewayServiceApplication.java
@@ -3,12 +3,9 @@ package com.clumsycoder.controlshift.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-		org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class GatewayServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 }
